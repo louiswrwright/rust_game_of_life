1a2d056c7a96cf1c3e6020db0537f89cb59fda37
Tom Meader <tom.meader@esendex.com>
2018-08-26 21:53:13 +0100

    get hype

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,28 @@ struct Cell {
 impl Sized1 for Cell {}
 
 fn main() {
-    use std::io::{stdin,stdout,Write};
-    let mut array_length = String::new();
-    print!("Enter grid length");
-    let _ = stdout().flush();
-    stdin().read_line(&mut array_length);
-
-    let mut array_width = String::new();
-    print!("Enter grid width");
-    let _ = stdout().flush();
-    stdin().read_line(&mut array_width);
-    print!("{}", array_length);
-
+//    use std::io::{stdin,stdout,Write};
+//    let mut array_length = String::new();
+//    print!("Enter grid length");
+//    let _ = stdout().flush();
+//    stdin().read_line(&mut array_length);
+//
+//    let mut array_width = String::new();
+//    print!("Enter grid width");
+//    let _ = stdout().flush();
+//    stdin().read_line(&mut array_width);
+//    print!("{}", array_length);
     let mut grid = [[Cell {on: false}; 8]; 8];
 
+    // "block"
+    grid[4][4].on = true;
+    grid[4][5].on = true;
+    grid[3][5].on = true;
+
+    // "blinker"
+    grid[1][0].on = true;
+    grid[1][1].on = true;
+    grid[1][2].on = true;
     start_game(grid);
 }
 
@@ -28,28 +36,83 @@ fn initialize_grid(start_position_x: i16, start_position_y: i16){
 
 }
 
-fn start_game(grid: [[Cell; 8]; 8]) {
+fn start_game(mut grid: [[Cell; 8]; 8]) {
     use std::{thread};
-    initialize_grid(4,4);
+//    initialize_grid(4,4);
 
-//    while true == true {
-        game_tick(grid);
-        thread::sleep_ms(250);
-//    }
+    print_grid(&grid);
+    println!();
+    while true == true {
+        grid = game_tick(grid);
+
+        print_grid(&grid);
+        println!();
+        thread::sleep_ms(1000);
+    }
 }
 
-fn game_tick(mut grid: [[Cell; 8]; 8]) {
-    let second_grid = [[Cell {on: false}; 8]; 8];
-    grid.clone_from_slice(&second_grid);
+fn game_tick(mut grid: [[Cell; 8]; 8]) -> [[Cell; 8]; 8]{
+    let mut second_grid = [[Cell { on: false }; 8]; 8];
 
-    for (index, row) in second_grid.iter().enumerate() {
+    for (index, row) in grid.iter().enumerate() {
         for (sub_index, cell) in row.iter().enumerate() {
-            println!("Index 1: {}, Index 2: {}", index, sub_index);
+            let count = count_neighbours(&grid, index, sub_index);
+            let current_cell_alive = grid[index][sub_index].on;
+
+            if count < 2 && current_cell_alive {
+                second_grid[index][sub_index].on = false;
+            } else if (count == 2 || count == 3) && current_cell_alive {
+                second_grid[index][sub_index].on = true;
+            } else if count == 3 && !current_cell_alive {
+                second_grid[index][sub_index].on = true;
+            } else if count > 3 && current_cell_alive {
+                second_grid[index][sub_index].on = false;
+            }
         }
     }
-
+    second_grid
 }
 
-fn fewer_than_two_live_neighbours(grid: [[Cell; 8]; 8], x: i8, y: i8){
-    
+fn print_grid(grid: &[[Cell; 8]; 8]){
+    for (index, row) in grid.iter().enumerate() {
+        for (sub_index, cell) in row.iter().enumerate() {
+            if grid[index][sub_index].on {
+                print!("*");
+            }
+            else {
+                print!(".");
+            }
+        }
+        println!()
+    }
+}
+
+fn count_neighbours(grid: &[[Cell; 8]; 8], x: usize, y: usize) -> i32 {
+    let mut count = 0;
+
+    if x > 0 && y > 0 && grid[x-1][y-1].on {
+        count+=1;
+    }
+    if y > 0 && grid[x][y-1].on {
+        count+=1;
+    }
+    if x < 7 && y > 0 && grid[x+1][y-1].on {
+        count+=1;
+    }
+    if x > 0 && grid[x-1][y].on {
+        count+=1;
+    }
+    if x < 7 && grid[x+1][y].on {
+        count+=1;
+    }
+    if x > 0 && y < 7 && grid[x-1][y+1].on {
+        count+=1;
+    }
+    if y < 7 && grid[x][y+1].on {
+        count+=1;
+    }
+    if x < 7 && y < 7 && grid[x+1][y+1].on {
+        count+=1;
+    }
+    count
 }
